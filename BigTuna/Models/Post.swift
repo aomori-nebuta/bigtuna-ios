703dd605814f9c1a41beb5a8d8162ab8727c6fa8
James Wu <therealjameswu@gmail.com>
2019-01-07 21:29:29 -0500

    Add post and restaurant models

---
+++ BigTuna/Models/Post.swift
@@ -0,0 +1,38 @@
+//
+//  Post.swift
+//  BigTuna
+//
+//  Created by James Wu on 1/7/19.
+//  Copyright © 2019 aomori nebuta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Post {
+    var uploader: User
+    var recommended: Bool
+    var postImage: UIImage
+    
+    var priceRange: PriceRange
+    
+    var menuItems: [String]
+    var tags: [String]
+    
+    var rating: Rating
+    
+    var comment: String
+    
+    var uploadDate: Date
+    var lastEdited: Date
+    
+    
+}
+
+enum PriceRange: Int {
+    case cheap = 1, moderate, costly, expensive
+}
+
+enum Rating: Int {
+    case worst = 1, bland, satisfactory, great, best
+}
---
+++ BigTuna/Models/Restaurant.swift
@@ -0,0 +1,9 @@
+//
+//  Restaurant.swift
+//  BigTuna
+//
+//  Created by James Wu on 1/7/19.
+//  Copyright © 2019 aomori nebuta. All rights reserved.
+//
+
+import Foundation
