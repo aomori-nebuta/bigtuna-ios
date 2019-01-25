e73355872386845fdff7513a208fc7cfae372aaa
James Wu <therealjameswu@gmail.com>
2019-01-24 21:45:48 -0500

    Switch between segmented control views

--- BigTuna/View Controllers/LoginViewController.swift
+++ BigTuna/View Controllers/LoginViewController.swift
@@ -7,16 +7,62 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var loginSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var signInView: UIView!
+    @IBOutlet weak var signUpView: UIView!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        updateUI()
     }
     
+    @IBAction func updateLoginSegmentedControl(_ sender: UISegmentedControl) {
+        updateUI()
+    }
+    
+    func updateUI() {
+        switch(loginSegmentedControl.selectedSegmentIndex) {
+        case 0:
+            signInView.isHidden = false
+            signUpView.isHidden = true
+        case 1:
+            signUpView.isHidden = false
+            signInView.isHidden = true
+        default:
+            break;
+        }
+    }
+    
+    @IBAction func signUp() {
+        guard let username = usernameTextField.text,
+        let email = emailTextField.text,
+        let password = passwordTextField.text,
+            let confirmPassword = confirmPasswordTextField.text else {
+                // TODO: Display message to user
+                print("Did not save user!")
+                return
+        }
+        
+        // TODO: Validate password here...
 
+        Auth.auth().createUser(withEmail: email, password: password) {
+            (authResult, error) in
+            guard let user = authResult?.user else { return }
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
