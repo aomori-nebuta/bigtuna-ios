694fe4440c0f91b00ad883ffb1c5133d537e7614
James Wu <therealjameswu@gmail.com>
2019-03-16 17:13:38 -0400

    Set window's rootViewController to be LoginViewController

--- BigTuna/AppDelegate.swift
+++ BigTuna/AppDelegate.swift
@@ -18,6 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = LoginViewController()
+        
         return true
     }
 
