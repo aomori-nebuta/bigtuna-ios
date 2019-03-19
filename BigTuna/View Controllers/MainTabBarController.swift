720a006cd3966c548af3ec0a420e7cf170471777
James Wu <therealjameswu@gmail.com>
2019-03-18 21:14:54 -0400

    Update credits comment

--- BigTuna/View Controllers/MainTabBarController.swift
+++ BigTuna/View Controllers/MainTabBarController.swift
@@ -30,7 +30,7 @@ class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
     func displayCameraActionSheet() {
         let cameraOptionMenu = UIAlertController(title: "Upload Image From", message: nil, preferredStyle: .actionSheet)
         // Safe for determining popover presentations for iPhone or iPad
-        // Based off of: https://stackoverflow.com/q/24224916
+        // Based on https://stackoverflow.com/q/24224916
         if let popoverPresentationController = cameraOptionMenu.popoverPresentationController {
             popoverPresentationController.sourceView = self.view
             popoverPresentationController.sourceRect = CGRect(x: self.tabBar.frame.width, y: (self.view.bounds.maxY - self.tabBar.frame.height), width: 0, height: 0)
