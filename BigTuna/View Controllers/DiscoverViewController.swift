15533d65f61f2c28f77413b5dbab582dbc144862
James Wu <therealjameswu@gmail.com>
2019-03-23 17:46:12 -0400

    Add collection view controller to discover tab

--- BigTuna/View Controllers/DiscoverViewController.swift
+++ BigTuna/View Controllers/DiscoverViewController.swift
@@ -8,23 +8,43 @@
 
 import UIKit
 
-class DiscoverViewController: UIViewController {
+class DiscoverViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    private let categoryCellIdentifier = "CategoryCell"
+    
+    let data = ["one", "two", "three"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        self.title = "Discover Page"
+        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: categoryCellIdentifier)
+        collectionView.alwaysBounceVertical = true
+        collectionView.backgroundColor = .green
+        
+        setupViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupViews() {
+//        collectionView.translatesAutoresizingMaskIntoConstraints = false
+//        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
     }
-    */
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: categoryCellIdentifier, for: indexPath) as! CategoryCell
+        print(indexPath.row)
+        cell.label.text = data[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height * 0.2)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
 
 }
