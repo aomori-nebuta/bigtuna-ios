38e89d9eff60c0e5185c5db6273fa8ee547f47c9
James Wu <therealjameswu@gmail.com>
2019-03-23 17:45:43 -0400

    Place post cells within Category cells

--- BigTuna/Views/CategoryCell.swift
+++ BigTuna/Views/CategoryCell.swift
@@ -6,4 +6,81 @@
 //  Copyright © 2019 aomori nebuta. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CategoryCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    private let postCellIdentifier = "PostCell"
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Required init not implemented for PostCell!")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .purple
+        setupViews()
+    }
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let postsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        return collectionView
+    }()
+    
+    func setupViews() {
+        // TODO: Try and use a UIView to center everything :-)
+
+        addSubview(postsCollectionView)
+        
+        postsCollectionView.dataSource = self
+        postsCollectionView.delegate = self
+        
+        postsCollectionView.register(PostCell.self, forCellWithReuseIdentifier: postCellIdentifier)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": postsCollectionView]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": postsCollectionView]))
+    }
+    
+    // Conforms to protocol UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    // Conforms to protocol UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: postCellIdentifier, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        return CGSize(width: frame.width * 0.3, height: frame.height)
+    }
+    
+}
+
+class PostCell: UICollectionViewCell {
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Required init not implemented for PostCell!")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .purple
+        setupViews()
+    }
+    
+    func setupViews() {
+        backgroundColor = .brown
+    }
+}
