1c2518513e78e149c1f5e763485097baea469b44
James Wu <therealjameswu@gmail.com>
2019-03-24 12:35:59 -0400

    Remove nested collection view

--- BigTuna/Views/CategoryCell.swift
+++ BigTuna/Views/CategoryCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CategoryCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class CategoryCell: UICollectionViewCell, UICollectionViewDelegateFlowLayout {
 
     let data = [["bob", "joe"], ["sally", "rob"], ["jill"]]
     
@@ -30,42 +30,7 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDelegate, UICollection
         return label
     }()
     
-    let postsCollectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .white
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-
-        return collectionView
-    }()
-    
     func setupViews() {
-        // TODO: Try and use a UIView to center everything :-)
 
-        addSubview(postsCollectionView)
-        
-        postsCollectionView.dataSource = self
-        postsCollectionView.delegate = self
-        
-        postsCollectionView.register(PostCell.self, forCellWithReuseIdentifier: postCellIdentifier)
-        
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": postsCollectionView]))
-        
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": postsCollectionView]))
-    }
-    
-    // Conforms to protocol UICollectionViewDataSource
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    // Conforms to protocol UICollectionViewDataSource
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: postCellIdentifier, for: indexPath)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        return CGSize(width: frame.width * 0.3, height: frame.height)
     }
 }
