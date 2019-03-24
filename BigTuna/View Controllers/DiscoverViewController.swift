a393b79f37a9e4e78a056b1f2412b5b8338c7b9b
James Wu <therealjameswu@gmail.com>
2019-03-24 12:07:20 -0400

    Add dynamic grid layout to Discover View Controller

--- BigTuna/View Controllers/DiscoverViewController.swift
+++ BigTuna/View Controllers/DiscoverViewController.swift
@@ -10,41 +10,70 @@ import UIKit
 
 class DiscoverViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
-    private let categoryCellIdentifier = "CategoryCell"
+    private let postCellIdentifier = "PostCell"
     
     let data = ["one", "two", "three"]
+    
+    let estimatedWidth = 160.0
+    let cellMarginSize = 16.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         self.title = "Discover Page"
-        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: categoryCellIdentifier)
         collectionView.alwaysBounceVertical = true
         collectionView.backgroundColor = .green
         
-        setupViews()
+        collectionView.dataSource = self
+        collectionView.register(PostCell.self, forCellWithReuseIdentifier: postCellIdentifier)
+        
+        setupGridView()
     }
     
-    func setupViews() {
-//        collectionView.translatesAutoresizingMaskIntoConstraints = false
-//        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.setupGridView()
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+    }
+    
+    func setupGridView() {
+        let flow = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let inset = view.frame.width * 0.035
+        flow.sectionInset = UIEdgeInsets(top: inset , left: inset, bottom: inset, right: inset)
+        flow.minimumInteritemSpacing = CGFloat(cellMarginSize)
+        flow.minimumLineSpacing = CGFloat(cellMarginSize)
     }
     
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: categoryCellIdentifier, for: indexPath) as! CategoryCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: postCellIdentifier, for: indexPath) as! PostCell
         print(indexPath.row)
-        cell.label.text = data[indexPath.row]
+        cell.setData(text: data[indexPath.row])
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height * 0.2)
+        let width = calculateWidth()
+        return CGSize(width: width, height: width)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return data.count
+    }
+    
+    // Based on https://www.youtube.com/watch?v=FMqX628vE1c
+    func calculateWidth() -> CGFloat {
+        let estimateWidth = CGFloat(estimatedWidth)
+        let cellCount = floor(CGFloat(view.frame.size.width / estimateWidth))
+        
+        let margin = CGFloat(cellMarginSize * 2)
+        let width = (view.frame.size.width - (CGFloat(cellMarginSize) * (cellCount - 1)) - margin) / cellCount
+
+        return width
     }
 
 }
